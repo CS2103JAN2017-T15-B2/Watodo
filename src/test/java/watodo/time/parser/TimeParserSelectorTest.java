2d4d66616a46cff5799a3a5baf731c730489b512
He Xing Darius <E0007888@u.nus.edu>
2017-04-08 23:45:35 +0800

    Add tests for TimeParserSelector #2

--- src/test/java/watodo/time/parser/TimeParserSelectorTest.java
+++ src/test/java/watodo/time/parser/TimeParserSelectorTest.java
@@ -10,40 +10,40 @@ public class TimeParserSelectorTest {
 
     @Test
     public void applicableTimeFormat() {
-        
+
         StandardDateTimeParser standard = new StandardDateTimeParser();
         ISODateTimeParser iso = new ISODateTimeParser();
         TodayTimeParser today = new TodayTimeParser();
         TomorrowTimeParser tmr = new TomorrowTimeParser();
-        
+
         //valid format
         assertTrue(standard.applyTo("16 Oct 2016 5.00pm"));
         assertTrue(standard.applyTo("30 jan 2017 1.59pm"));
-        
+
         assertTrue(iso.applyTo("2017-09-23 8.00am"));
         assertTrue(iso.applyTo("1996-01-07 1.00am"));
-        
+
         assertTrue(today.applyTo("today 12.01am"));
         assertTrue(today.applyTo("today 11.59pm"));
-        
+
         assertTrue(tmr.applyTo("tmr 10.00am"));
         assertTrue(tmr.applyTo("tmr 4.00pm"));
-        
+
         //invalid formats
         assertFalse(standard.applyTo("16-Oct-2016 5.00pm"));
         assertFalse(standard.applyTo("30 january 2017 1.59pm"));
         assertFalse(standard.applyTo("30 jan 2017 01:59"));
         assertFalse(standard.applyTo("30 jan 2017 00:09"));
-        
+
         assertFalse(iso.applyTo("2017/09/23 8.00am"));
         assertFalse(iso.applyTo("1996-01-07 1:00"));
-        
+
         assertFalse(today.applyTo("today 12:01"));
         assertFalse(today.applyTo("today 13.59pm"));
-        
+
         assertFalse(tmr.applyTo("tmr 1000"));
         assertFalse(tmr.applyTo("tmr 16:00"));
-        
+
     }
-    
+
 }
